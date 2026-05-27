1aaee2b473df55c1ca01ec6dfc935b1cc576bb91
m-j0401 <mehul.jain2206@gmail.com>
2026-05-27 18:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+let sqr=document.querySelectorAll(".square");
+
+for(let i=0;i<sqr.length;i++ )
+	{
+		sqr[i].addEventListener("mouseover",()=>{
+        let ind=i;
+       changeColor(ind)
+		})
+
+
+	}
+function changeColor(ind) {
+	for(let i=0;i<sqr.length;i++)
+		{
+			if(i==ind)
+			{
+				continue;
+			}
+			else
+			{
+				sqr[i].style.backgroundColor="#6F4E37"
+			}
+		}
+}
+
+
+
+
